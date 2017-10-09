985049fb464a4ad8642f1239b0c9dac330407428
Thipok Cholsaipant <thipok.ch@gmail.com>
2017-10-08 17:00:35 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Thipok Cholsaipant on 10/8/17.
+//  Copyright © 2017 Thipok Cholsaipant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
